5a0ea2de652246ffec17cbeaf394c98b3ef2b2f8
Kajasoruban <kajan.bluwin@gmail.com>
2023-11-10 16:04:32 +0530

    nav complete

--- script.js
+++ script.js
@@ -2,19 +2,31 @@
 var modes= document.getElementById("mode");
 var bgcolor=document.getElementById("bodyc");
 var logcol=document.getElementById("logofun");
+var menuc=document.getElementById("menu");
+var ull=document.getElementById("ull");
+var bla=document.getElementById("blank");
 
 
 function modeChange(imgs){
     if(imgs.match(/moon/i)){
         modes.src="Assets/img/Icons/sun.svg";
+        menuc.src="Assets/img/Icons/menu (1).svg";
     }else{
         modes.src="Assets/img/Icons/moon(dark).svg";
+        menuc.src="Assets/img/Icons/menu.svg";
     }
-    console.log(imgs);
+    
     
     bgcolor.classList.toggle("importand");
     logcol.classList.toggle("logocol2");
     
 }
 
+function menu(){
+    
+    ull.classList.toggle("ullc");
+    menuc.classList.toggle("men");
+    bla.classList.toggle("blankon");
+     
+}
 
